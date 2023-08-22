cafb3f8309593b4e67a0be1325751f965112943a
nicekim2000 <rlawogur0208@naver.com>
2023-08-23 01:42:11 +0900

    plz1

--- src/main/java/me/shinsunyoung/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/SpringBootDeveloperApplication.java
@@ -7,8 +7,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1p1beta1.SpeechSettings;
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.FileInputStream;
@@ -65,6 +72,43 @@ public class SpringBootDeveloperApplication {
                 return audioContents;
             }
         }
+
+
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
     }
     private static Credentials getCredentials() throws Exception {
         FileInputStream credentialsStream = new FileInputStream("./vaulted-fort-358506-3f5080aabb57.json");
--- src/main/java/me/shinsunyoung/config/SecurityConfig.java
+++ src/main/java/me/shinsunyoung/config/SecurityConfig.java
@@ -14,11 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-//        http.csrf().disable();
-//        http.authorizeHttpRequests().requestMatchers("/.well-known/**").permitAll();
-//        http.authorizeHttpRequests().requestMatchers("/convert").permitAll();
-//        http.authorizeHttpRequests().requestMatchers("/api/persons/insert").permitAll();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.csrf().disable();
+        http.authorizeHttpRequests().requestMatchers("/.well-known/**").permitAll();
+        http.authorizeHttpRequests().requestMatchers("/convert").permitAll();
+        http.authorizeHttpRequests().requestMatchers("/api/persons/insert").permitAll();
+        http.authorizeHttpRequests().requestMatchers("/").permitAll();
+//        http.authorizeHttpRequests().anyRequest().permitAll();
         return http.build();
     }
 }
