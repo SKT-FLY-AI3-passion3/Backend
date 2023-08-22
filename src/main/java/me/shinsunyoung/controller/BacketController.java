465d7b26ceece9f5f21aa38b639939427f599e41
nicekim2000 <rlawogur0208@naver.com>
2023-08-22 12:08:15 +0900

    https cert

--- src/main/java/me/shinsunyoung/controller/BacketController.java
+++ src/main/java/me/shinsunyoung/controller/BacketController.java
@@ -4,6 +4,8 @@ import me.shinsunyoung.Entity.BacketEntity;
 import me.shinsunyoung.Service.BacketService;
 import me.shinsunyoung.dto.BacketDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,8 +21,9 @@ public class BacketController {
 
 
     @PostMapping("/insert")
-    public BacketEntity insertBacket(@RequestBody BacketDTO dto) {
-        return backetService.insertBacket(dto);
+    public ResponseEntity<BacketEntity> insertBacket(@RequestBody BacketDTO dto) {
+        BacketEntity entity = backetService.insertBacket(dto);
+        return new ResponseEntity<>(entity, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
