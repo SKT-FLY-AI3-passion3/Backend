8d19c3ff15173e229c61f5c7d999641d845919ef
Nayeon-Ryu <2014027@sookmyung.ac.kr>
2023-08-22 17:33:19 +0900

    [feat]: TTS

--- src/main/java/me/shinsunyoung/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/SpringBootDeveloperApplication.java
@@ -1,13 +1,73 @@
 package me.shinsunyoung;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.speech.v1p1beta1.SpeechSettings;
+import com.google.cloud.texttospeech.v1.*;
+import com.google.protobuf.ByteString;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import static java.awt.SystemColor.text;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
+        synthesizeText("안녕하세요. 만나서 반가워요. 찾으시는 곳이 여기 맞나요?");
+    }
+
+    /**
+     * Demonstrates using the Text to Speech client to synthesize text or ssml.
+     *
+     * @param text the raw text to be synthesized. (e.g., "Hello there!")
+     * @throws Exception on TextToSpeechClient Errors.
+     */
+    public static ByteString synthesizeText(String text) throws Exception {
+        CredentialsProvider credentialsProvider = FixedCredentialsProvider.create(getCredentials());
+        // Instantiates a client
+        try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(TextToSpeechSettings.newBuilder().setCredentialsProvider(credentialsProvider).build())) {
+            // Set the text input to be synthesized
+            SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+            // Build the voice request
+            VoiceSelectionParams voice =
+                    VoiceSelectionParams.newBuilder()
+                            .setLanguageCode("ko-KR") // languageCode = "en_us"
+                            .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
+                            .build();
+
+            // Select the type of audio file you want returned
+            AudioConfig audioConfig =
+                    AudioConfig.newBuilder()
+                            .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
+                            .build();
+
+            // Perform the text-to-speech request
+            SynthesizeSpeechResponse response =
+                    textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+
+            // Get the audio contents from the response
+            ByteString audioContents = response.getAudioContent();
+
+            // Write the response to the output file.
+            try (OutputStream out = new FileOutputStream("output.mp3")) {
+                out.write(audioContents.toByteArray());
+                System.out.println("Audio content written to file \"output.mp3\"");
+                return audioContents;
+            }
+        }
+    }
+    private static Credentials getCredentials() throws Exception {
+        FileInputStream credentialsStream = new FileInputStream("./vaulted-fort-358506-3f5080aabb57.json");
+        return GoogleCredentials.fromStream(credentialsStream);
     }
 }
