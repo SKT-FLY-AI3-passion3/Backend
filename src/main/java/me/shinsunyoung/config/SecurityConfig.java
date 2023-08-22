8e02ea566dbb18044c5826a06c851fd0329859b7
nicekim2000 <rlawogur0208@naver.com>
2023-08-23 01:05:05 +0900

    plz

--- src/main/java/me/shinsunyoung/config/SecurityConfig.java
+++ src/main/java/me/shinsunyoung/config/SecurityConfig.java
@@ -13,12 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 //        http.csrf().disable();
-        http.authorizeHttpRequests().requestMatchers("/.well-known/**").permitAll();
-        http.authorizeHttpRequests().requestMatchers("/convert").permitAll();
-        http.authorizeHttpRequests().requestMatchers("/").permitAll();
-        http.authorizeHttpRequests().requestMatchers("/api/persons/insert").permitAll();
-//        http.authorizeHttpRequests().anyRequest().authenticated();
+//        http.authorizeHttpRequests().requestMatchers("/.well-known/**").permitAll();
+//        http.authorizeHttpRequests().requestMatchers("/convert").permitAll();
+//        http.authorizeHttpRequests().requestMatchers("/api/persons/insert").permitAll();
+        http.authorizeHttpRequests().anyRequest().permitAll();
         return http.build();
     }
 }
